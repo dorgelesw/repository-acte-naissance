2d2d625212e18d32d3f8880ac98169d4f7a1b3e2
p7cSarl <marreldorgeles@gmail.com>
2015-08-19 09:02:20 +0200

    sgan:0.0.1:initialisation du depot de developpement

---
+++ sgan/GEST_ACT_NAIS/Workflow/App_Start/IdentityConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(P7GestAct.IdentityConfig))]
+namespace P7GestAct
+{
+    public class IdentityConfig
+    {
+        public void Configuration(IAppBuilder app) { }
+
+    }
+
+
+}
---
+++ sgan/GEST_ACT_NAIS/Workflow/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="P7GestAct.Global" Language="C#" %>
