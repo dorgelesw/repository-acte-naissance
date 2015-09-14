ec3f7d79b5bc285a39c6c6cc9f982a92edc67ab9
kouatcho <kouabilly@gmail.com>
2015-09-14 15:11:47 +0100

    arrangement de la disposition des formulaires sur la vue index

--- sgan/GEST_ACT_NAIS/Workflow/Controllers/api/PersonnesController.cs
+++ sgan/GEST_ACT_NAIS/Workflow/Controllers/api/PersonnesController.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
+using System
+.Net.Http;
 using System.Web.Http;
 using P7GestAct.Models.Repositories;
 using P7GestAct.Models.DataClass;
--- sgan/GEST_ACT_NAIS/Workflow/Scripts/AfficherFormulaire.js
+++ sgan/GEST_ACT_NAIS/Workflow/Scripts/AfficherFormulaire.js
@@ -3,13 +3,18 @@
 
     $("#declarer").click(function () {
         
-        document.getElementById("declarant").style.visibility = "visible";
+        /*document.getElementById("declarant").style.visibility = "visible";
         document.getElementById("declaration").style.visibility = "hidden";
         document.getElementById("naissance").style.visibility = "hidden";
         document.getElementById("mere").style.visibility = "hidden";
         document.getElementById("pere").style.visibility = "hidden";
         //document.getElementById("last").style.visibility = "visible";
-        $("#").removeAttr('href').off('click');
+        $("#").removeAttr('href').off('click');*/
+        $("#declarant").css('display', 'block');
+        $("#declaration").css('display', 'none');
+        $("#naissance").css('display', 'none');
+        $("#mere").css('display', 'none');
+        $("#pere").css('display', 'none');
     });
    
    
